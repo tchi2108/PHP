e52d2c27a55e926429b361b708e3192455e6cdd6
tchi2108 <dothuychi2006@gmail.com>
2025-04-11 21:31:42 +0700

    DBInfo.php

---
+++ DBInfo.php
@@ -0,0 +1,24 @@
+<?php
+class DatabaseInfo
+{
+    public static function getServer()
+    {
+        return "localhost";
+    }
+    public static function getDatabaseName()
+    {
+        return "data2025";
+    }
+    public static function getUserName()
+    {
+        return "root";
+    }
+    public static function getPassword()
+    {
+        return "";
+    }
+    public static function getPort()
+    {
+        return "4306";
+    }
+}
